8b135522e3e9b7f1be1a6fe7ad3122f00f508fea
blokos <76009267+blokos0@users.noreply.github.com>
2025-03-23 02:43:32 +0300

    the copyright infringement commit...

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,13 +1,13 @@
 extends Node2D
-var bpm: float = 80
+var bpm: float = 183
 var timesec: float # normal time
 var timebeat: float # aligned to the beat
 var timebeatprev: float = -1
-var chart: PackedFloat32Array = [0, 0.25, 0.5, 0.75, 3.125, 3.25, 3.5, 3.75, 4, 4.25, 4.5, 4.75, 6.5, 7, 7.5, 8, 8.5, 8.75]
+var chart: PackedFloat32Array = [1, 2, 3, 4, 5, 6, 6.5, 7, 7.5, 8, 9, 10, 11, 11.5, 12, 13, 14, 15, 15.5, 16, 17, 18, 19, 19.5, 20, 21, 22, 23, 23.5, 24, 24.5, 25, 25.5, 26, 26.5, 27, 27.5, 28, 28.5, 29, 29.5, 30, 30.5, 31, 31.5, 32]
 var chartnext: int # index of the next chart note
 var charthit: int # index of the last hit note
 func _ready() -> void:
-	$music.stream = ResourceImporterOggVorbis.load_from_file("C:/Users/User/Music/interesting.ogg")
+	$music.stream = ResourceImporterOggVorbis.load_from_file("music/heatabnormal.ogg")
 	$music.play()
 func _process(delta: float) -> void:
 	if floorf(timebeat) != floorf(timebeatprev):
@@ -15,13 +15,12 @@ func _process(delta: float) -> void:
 	timesec += delta
 	timebeatprev = timebeat
 	timebeat = timesec / (60 / bpm)
-	#$/root.position = Vector2(704, 396 - abs(sin(timebeat * PI / 2) * 64))
+	#$/root.position = Vector2(704, 396 - abs(sin(timebeat * PI / 4) * 32))
 	if chartnext < len(chart):
 		if chart[chartnext] < timebeat:
 			chartnext += 1
 	else:
-		OS.alert("done")
-		get_tree().quit()
+		get_tree().reload_current_scene()
 	queue_redraw()
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey && event.pressed && !event.echo:
@@ -56,8 +55,8 @@ func beat() -> void:
 	pass # something will probably happen here
 func _draw() -> void:
 	var ringouterrad: float = 112
-	var ringinnerrad: float = 82
-	var playerrad: float = 15
+	var ringinnerrad: float = 82 + sin(timebeat * PI / 4) * 15
+	var playerrad: float = (ringouterrad - ringinnerrad) / 2
 	draw_circle(Vector2(256, 144), ringouterrad, Color.WHITE, false, 2)
 	draw_circle(Vector2(256, 144), ringinnerrad, Color.WHITE, false, 2)
 	draw_circle(Vector2(256 + sin(timebeat * PI / 2) * (ringouterrad - playerrad), 144 + cos(timebeat * PI / 2) * (ringouterrad - playerrad)), playerrad, Color.WHITE, false, 2)
